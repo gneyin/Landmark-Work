34b28249570fb1a94ffa882c86e05a3e9ab16c46
Gregory Neyin <gregory.neyin@gmail.com>
2021-08-27 23:44:46 +0000

    2nd commit

---
+++ rice.sh
@@ -0,0 +1,8 @@
+vi nameandage.sh
+#!/bin/bash
+# This script accepts the name and age of a user and displays it
+echo "Please enter name"
+read name
+echo "Please enter age"
+read age
+echo " I am $name and my age is $age"
---
+++ toyota.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Enter Name"
+read name
+echo "Age"
+read age
+echo "I am $name and I am $age years old"
+
